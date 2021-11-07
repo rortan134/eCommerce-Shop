a1e35c920bf400f69da30cfc5946dbbbfbad9c8f
Rortan <samaritanojr006@gmail.com>
2021-11-07 20:40:04 +0100

    fix: fix github pages routing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 // Dependencies
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; 
 import { CommerceProvider } from "./components/shared/commerce-context";
 import "normalize.css";
-
 
 import "./index.scss";
 import App from "./components/App";
 
+// For github pages broken react-routing
+import { HashRouter } from "react-router-dom"; 
+//============================================
+
 ReactDOM.render(
     <CommerceProvider>
-        <Router basename={process.env.PUBLIC_URL}>
+        {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+        <HashRouter>
             <App />
-        </Router>
+        </HashRouter>
+        {/* </BrowserRouter> */}
     </CommerceProvider>,
     document.getElementById("root")
 );
